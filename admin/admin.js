3850a0bd6ca3f3a52db51cba75c15d62055c9e01
pilotaintour <pilotaintour13@gmail.com>
2026-09-24 00:14:49 +0300

    Rimossi i pulsanti preset fasce e mantenuto il solo pulsante di pulizia orari

--- admin/admin.js
+++ admin/admin.js
@@ -178,27 +178,10 @@ function toggleOrario(timeStr) {
 }
 
 function applicaPresetOrari(tipo) {
-    let presetSlots = [];
-    if (tipo === 'mattina') {
-        presetSlots = ['09:00', '09:15', '09:30', '09:45', '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30'];
-    } else if (tipo === 'pomeriggio') {
-        presetSlots = ['15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:15', '17:30', '17:45', '18:00', '18:15', '18:30', '18:45', '19:00', '19:15', '19:30'];
-    } else if (tipo === 'sera') {
-        presetSlots = ['20:00', '20:15', '20:30', '20:45', '21:00', '21:15', '21:30', '21:45', '22:00', '22:15', '22:30'];
-    } else if (tipo === 'reset') {
+    if (tipo === 'reset') {
         orariCorrenti = [];
         renderCampiOrari();
-        return;
     }
-
-    presetSlots.forEach(t => {
-        if (!orariCorrenti.includes(t)) {
-            orariCorrenti.push(t);
-        }
-    });
-
-    orariCorrenti.sort();
-    renderCampiOrari();
 }
 
 // Gestione Dynamic Tappe Input Builder
